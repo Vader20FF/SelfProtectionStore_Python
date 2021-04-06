4d670cd314e35c88f54d92e686935cadc087df05
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-04-06 18:28:08 +0200

    - Added Order tests

--- Order/Order.py
+++ Order/Order.py
@@ -3,12 +3,12 @@ import uuid
 
 
 class Order:
-    def __init__(self, client, products_list, shipping_method):
+    def __init__(self, client, __shopping_cart, shipping_method):
         self.__order_id = uuid.uuid4()
         self.__order_date = dt.datetime.today()
         self.__estimated_delivery_date = dt.date(dt.date.today().year, dt.date.today().month, dt.date.today().day + 2)
         self.__client = client
-        self.__products_list = products_list
+        self.__shopping_cart = __shopping_cart
         self.__shipping_method = shipping_method
         self.__total_cost = 0
         self.__status = "PLACED"
@@ -26,14 +26,14 @@ class Order:
     def get_client(self):
         return self.__client
 
-    def get_products_list(self):
-        return self.__products_list
+    def get_shopping_cart(self):
+        return self.__shopping_cart
 
     def get_shipping_method(self):
         return self.__shipping_method
 
     def get_total_cost(self):
-        return self.__total_cost
+        return self.get_shopping_cart().get_shopping_cart_total_cost()
 
     def get_status(self):
         return self.__status
@@ -49,7 +49,8 @@ class Order:
     def get_order_info(self):
         return f"ORDER ID: {self.get_order_id()}\n" \
                f"ORDER DATE: {self.get_order_date()}\n" \
-               f"ESTIMATED DELIVERY DATE: {self.get_estimated_delivery_date()}\n" \
-               f"SHIPPING METHOD:\n{self.get_shipping_method()}\n\n" \
+               f"ESTIMATED DELIVERY DATE: {self.get_estimated_delivery_date()}\n\n" \
+               f"{self.get_shopping_cart().get_shopping_cart_info()}\n\n" \
+               f"SHIPPING METHOD: {self.get_shipping_method()}\n" \
                f"TOTAL COST: {self.get_total_cost()}\n" \
                f"STATUS: {self.get_status()}\n"
--- tests/test_order.py
+++ tests/test_order.py
@@ -3,49 +3,46 @@ from Product.ColdSteel.Knife import Knife
 from Product.ColdSteel.Machete import Machete
 from Product.Gun.PelletGun import PelletGun
 from Order.ShoppingCart import ShoppingCart
+from Order.Order import Order
+from Client.Client import Client
+from Address.ClientAddress import ClientAddress
+from Address.ShippingAddress import ShippingAddress
+import datetime as dt
 
 
-class TestShoppingCart(TestCase):
+class TestOrder(TestCase):
     def setUp(self):
+        self.client_address = ClientAddress("Ulica", "18", "11", "Miasto", "Kod pocztowy", "Polska")
+        self.shipping_address = ShippingAddress("Ulica2", '22', '23', "Miasto2", "Kod pocztowy 2", "Anglia")
+        self.client = Client("client_imie", "client_nazwisko", "client_email", "client_phone",
+                             self.client_address, self.shipping_address, False, 400)
         self.knife = Knife(23, "knife_manufacturer", "knife_name", 500, "knife_material", 20)
         self.machete = Machete(23, "machete_manufacturer", "machete_name", 500, "machete_material", "machete_color")
         self.pelletgun = PelletGun(23, "pelletgun_manufacturer", "pelletgun_name", 500, 30, ".30",
                                    "pelletgun_fire_rate")
         self.shoppingCart = ShoppingCart()
-
-    def test_add_product_to_shopping_cart(self):
-        self.shoppingCart.add_product_to_shopping_cart(self.knife)
-        self.assertEqual(len(self.shoppingCart.get_shopping_cart_products_list()), 1)
-
-    def test_remove_product_from_shopping_cart(self):
-        self.shoppingCart.add_product_to_shopping_cart(self.knife)
-        self.shoppingCart.remove_product_from_shopping_cart(self.knife)
-        self.assertEqual(len(self.shoppingCart.get_shopping_cart_products_list()), 0)
-
-    def test_make_shopping_cart_empty(self):
         self.shoppingCart.add_product_to_shopping_cart(self.knife)
         self.shoppingCart.add_product_to_shopping_cart(self.machete)
         self.shoppingCart.add_product_to_shopping_cart(self.pelletgun)
-        self.shoppingCart.make_shopping_cart_empty()
-        self.assertEqual(len(self.shoppingCart.get_shopping_cart_products_list()), 0)
+        self.order = Order(self.client, self.shoppingCart, "ZA POBRANIEM")
 
-    def test_get_shopping_cart_total_cost(self):
-        self.shoppingCart.add_product_to_shopping_cart(self.knife)
-        self.shoppingCart.add_product_to_shopping_cart(self.machete)
-        self.assertEqual(self.shoppingCart.get_shopping_cart_total_cost(), 1000)
+    def test_getter_and_setter(self):
+        self.order.set_status("WYSLANE")
+        self.assertEqual(self.order.get_status(), "WYSLANE")
 
-    def test_shopping_cart_products_list(self):
-        self.shoppingCart.add_product_to_shopping_cart(self.knife)
-        self.shoppingCart.add_product_to_shopping_cart(self.machete)
-        self.shoppingCart.add_product_to_shopping_cart(self.pelletgun)
-        self.assertEqual(self.shoppingCart.get_shopping_cart_products_list(),
-                         [self.knife, self.machete, self.pelletgun])
+        self.assertEqual(self.order.get_order_date(), dt.datetime.today())
+
+        self.assertEqual(self.order.get_estimated_delivery_date(),
+                         dt.date(dt.date.today().year, dt.date.today().month, dt.date.today().day + 2))
+
+        self.assertEqual(self.order.get_client(), self.client)
+
+        self.assertEqual(self.order.get_shopping_cart(), self.shoppingCart)
+
+        self.assertEqual(self.order.get_shipping_method(), "ZA POBRANIEM")
+
+        self.assertEqual(self.order.get_total_cost(), 1500)
 
         print()
-        print(self.shoppingCart.get_shopping_cart_info())
+        print(self.order.get_order_info())
         print()
-
-
-
-
-
